c299130f7b121989f5c117b526d5ee950ce95e01
PaMy <Usuario@2DAWPC07>
2015-11-12 14:11:00 +0100

    ficheros php de conexion y config.php

---
+++ aplicacionNoticias/conexion.php
@@ -0,0 +1,19 @@
+<?php
+$hostname='127.0.0.1';
+$database='noticias';
+
+$username = 'admin';
+$password = 'admin';
+
+try {
+$con = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+print "Conexión exitosa!";
+}
+catch (PDOException $e) {
+print "¡Error!: " . $e->getMessage() . "
+";
+die();
+}
+$con =null;
+
+?>

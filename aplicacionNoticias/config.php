7e4f457e6e0076fa797becc0da1caae488e7b0ae
Usuario <Usuario@2DAWPC07>
2015-11-13 09:53:43 +0100

    configuracion del fichero config.php 1.0

--- aplicacionNoticias/config.php
+++ aplicacionNoticias/config.php
@@ -1,11 +1,9 @@
 <?php
 include 'conexion.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$con=  conexion();
+$consulta="select * from usuario";
 
+$resultado=  mysqli_query($con, $consula);
 
 ?>

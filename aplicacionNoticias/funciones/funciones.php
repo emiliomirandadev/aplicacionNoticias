5ce3b0c4dde2075211be4ea089b988032427e94d
Usuario <Usuario@2DAWPC07>
2015-11-18 10:43:39 +0100

    subire mas veces no vaya ser que se pete D:

--- aplicacionNoticias/funciones/funciones.php
+++ aplicacionNoticias/funciones/funciones.php
@@ -14,8 +14,8 @@ function buscar_usuario($nombre, $pass) {
     $con=  conexion();
     /* La busqueda en la base de datos se realiza de este modo para evitar las inyecciones sql */
     $query = ("SELECT correo, passwd FROM usuario  WHERE correo = '$nombre' and passd='$pass'");
-    $resultado->query($query);
-    $nr=  $resultado->num_rows;
+    $resultado=$con->query($query);
+    $nr=$resultado->num_rows;
 
     if ($nr == 1) {
         return false;

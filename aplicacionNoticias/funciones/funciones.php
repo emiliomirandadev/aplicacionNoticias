92c7faa762d941740625b0cff1a3db6fc9ef6b5d
Usuario <Usuario@2DAWPC07>
2015-11-18 10:35:43 +0100

    listar usuarios para borrar

--- aplicacionNoticias/funciones/funciones.php
+++ aplicacionNoticias/funciones/funciones.php
@@ -14,7 +14,7 @@ function buscar_usuario($nombre, $pass) {
     $con=  conexion();
     /* La busqueda en la base de datos se realiza de este modo para evitar las inyecciones sql */
     $query = ("SELECT correo, passwd FROM usuario  WHERE correo = '$nombre' and passd='$pass'");
-    $resultado=  mysqli_query($con, $query);
+    $resultado->query($query);
     $nr=  $resultado->num_rows;
 
     if ($nr == 1) {

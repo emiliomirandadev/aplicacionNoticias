38032a791903c972774cc311c19a9d3db4e4c00d
Usuario <Usuario@2DAWPC07>
2015-11-18 10:58:32 +0100

    cagundeu

--- aplicacionNoticias/config.php
+++ aplicacionNoticias/config.php
@@ -4,17 +4,75 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/estilos.css">
         <title>Usuarios</title>
-    </head>
-    <body>
-        <?php include 'funciones/listar_usuarios_modificar.php' ?>
-        <header class="cabecera_config">
 
-            <input class="boton" type="button" value="Usuarios" onclick="<?php $r = mostrar_usuarios_mod() ?>">
-            <input class="boton" type="button" value="Noticias">
-        </header>
 
-        <?php
-        echo $r;
-        ?>
-    </body>
+    </style>
+
+    <!-- Referencias a Javascripts externos -->
+
+    <script src="http://www.google.com/jsapi"></script>
+    <script>google.load("jquery", "1");</script>
+
+    <!-- Código Javascript interno -->
+
+    <script type='text/javascript'>
+
+        /* Código a ejecutarse tan pronto como la
+         página ha sido cargada por el navegador */
+
+        $(document).ready(function ()
+        {
+            /* Asociar el evento de clic del botón 'igual'
+             con la lógica del negocio de la aplicación */
+
+            $('#igual').click(function ()
+            {
+                procesar();
+            });
+        });
+
+        function procesar()
+        {
+            $.ajax({
+                url: 'funciones.php', /* URL a invocar asíncronamente */
+                type: 'post', /* Método utilizado para el requerimiento */
+                data: $('#formulario').serialize(), /* Información local a enviarse con el requerimiento */
+
+
+            }
+            success: 	function(request, settings)
+				{
+					/* Cambiar el color del texto a verde */
+					
+					$('#mensaje').css('color', '#0ab53a');
+					
+					/* Mostrar un mensaje informando el éxito sucedido */
+					
+					$('#mensaje').html("Operación realizada exitosamente");
+					
+					/* Mostrar el resultado obtenido del cálculo solicitado */
+					
+					$('#resultado').html(request);
+				},
+        }
+        
+
+    </script>
+
+</head>
+<body>
+    <?php include 'funciones/listar_usuarios_modificar.php' ?>
+    <header class="cabecera_config">
+
+
+        <form name="a" action="#" method="post">
+            <input type='button' id="igual" value='Usuarios' />
+        </form>
+        <input class="boton" type="button" value="Noticias">
+    </header>
+    <span id='resultado'>&nbsp;</span>
+    <?php
+    echo $r;
+    ?>
+</body>
 </html>

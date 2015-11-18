8d7f9d15d092afe3bec94c064dfbf684fd539e1d
Usuario <Usuario@2DAWPC07>
2015-11-18 10:38:38 +0100

    listar usuarios para borrar 2

--- aplicacionNoticias/config.php
+++ aplicacionNoticias/config.php
@@ -1,33 +1,20 @@
 <html>
+    <?php $r = "no se ha seleccionado consulta"; ?>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="css/estilos.css">
         <title>Usuarios</title>
     </head>
     <body>
+        <?php include 'funciones/listar_usuarios_modificar.php' ?>
+        <header class="cabecera_config">
 
+            <input class="boton" type="button" value="Usuarios" onclick="<?php $r = mostrar_usuarios_mod() ?>">
+            <input class="boton" type="button" value="Noticias">
+        </header>
 
         <?php
-        include 'funciones/conexion.php';
-
-        $con = conexion();
-        $consulta = "select correo,nombre,apellido,telefono,grupo,curso from usuario";
-
-
-        $resultado = $con->query($consulta);
-        $r = array();
-        echo "<table class='tabla_usuarios' border='1'>";
-        while ($row = $resultado->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr>";
-            foreach ($row as $key => $value) {
-
-                echo "<td> $value </td> ";
-            }
-
-            echo "<td><form name='mod_usuario' action='usuario.php' method='POST'> <input type='submit' name='a' value='Modificar'></form> <form name='borrar_usuario' action='borrar_usuario.php' method='POST'><input type='submit' value='borrar'></form></td>";
-            echo "</tr>";
-        }
-        echo "</table>";
+        echo $r;
         ?>
     </body>
 </html>

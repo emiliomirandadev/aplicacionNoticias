c8f65b1b9d22d2042eb5a7b3792ce82d8f2ea19f
yobonico <gemilio@gmail.com>
2015-11-19 10:16:44 +0100

    index listo

--- aplicacionNoticias/index.php
+++ aplicacionNoticias/index.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/estilos.css">
-        <title>Inicio</title>
-    </head>
-    <body>
-      
-        <?php
-        include ('noticias.php');
-        if(isset($_COOKIE['conexion'])){
-            volver();
-        }
-        ?>
-    </body>
-</html>
+<?php
+if ((!isset($_COOKIE['conexion'])) || (is_null($_COOKIE['conexion']))) {
+    header('Location: login.php');
+} else
+    header ('Location: noticias.php');
+?>
